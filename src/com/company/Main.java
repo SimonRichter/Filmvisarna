055382b9d1b7675e1fa69555ba0e5e18a926fe36
snabelA00 <ja000411@gmail.com>
2021-01-11 16:58:10 +0100

    backend added

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import express.Express;
+import static express.database.Database.collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var app = new Express();
+
+        //app.enableCollections("database/temp/db/awesome.db");
+
+        app.get("/hello", (req, res) -> {
+            // send can only send a string/text
+            res.send("<h1>Hello from Java Express!</h1>");
+        });
+
+        // skapa endpoint/rout för att hämta alla marvels
+        app.get("/rest/marvel", (req, res) -> {
+            // find() = alla från collection
+            var marvel = collection("Marvel").find();
+            // stringifierar marvel lista. skickar tillbaka listan snyggt och prydligt
+            res.json(marvel);
+        });
+
+        //choose another port so it doesn't collide with VUE port
+        app.listen(5000);
+    }
+}
+
---
+++ src/com/company/models/Movie.java
@@ -0,0 +1,7 @@
+package com.company.models;
+import express.database.Model;
+import org.dizitart.no2.objects.Id;
+
+@Model
+public class Movie {
+}
