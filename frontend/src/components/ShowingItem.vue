4b8bd8f7f94c413c9879704dafe250df9bbb35f6
bextex <r.sarvegard@gmail.com>
2021-01-12 16:31:29 +0100

    Created showingItem and showingList as components

---
+++ frontend/src/components/ShowingItem.vue
@@ -0,0 +1,21 @@
+<!--
+1. Get movie index
+2. Get title of movie
+3. Get poster of movie
+4. Have a routerlink for the poster to get to chosen-movie-view
+-->
+
+<template>
+  <router-link :to="'/chosen-movie/' + movieIndex">
+    <img :src="movieUrl" />
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ["movieIndex,", "movieTitle", "movieUrl"],
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/ShowingList.vue
@@ -0,0 +1,18 @@
+<!--
+1. Import showingItem
+2. components: {showingItem}
+3. computed: { movies(){ get movies from store }} 
+4. v-for="movies" to show the movies in horizontal list
+-->
+
+<template>
+
+</template>
+
+<script>
+import ShowingItem from './ShowingItem.vue'
+export default {};
+</script>
+
+<style>
+</style>
