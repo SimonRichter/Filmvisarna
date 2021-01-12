824f163bac45f73c6e83770afbea0caa45929064
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-01-12 19:22:48 +0100

    Added member.json and code to Main
    JSON and CLASS for member with basic standard info added.
    
    Collection method to add and delete memebers and delete /cancel booking added.
    
    Co-Authored-By: Hugo Granath <72066319+mrHugolo@users.noreply.github.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import com.company.models.Movie;
 import com.company.models.Showing;
 import express.Express;
 import static express.database.Database.collection;
+import com.company.models.Member;
+
 
 public class Main {
 
@@ -15,25 +17,48 @@ public class Main {
         app.get("/hello", (req, res) -> res.send("<h1>Hello from Java Express!</h1>"));
 
         app.get("/rest/movies", (req, res) -> {
-
             var movies = collection(Movie.class).find();
-
             res.json(movies);
         });
 
         app.get("/rest/showings", (req, res) -> {
-
             var showings = collection(Showing.class).find();
-
             res.json(showings);
         });
 
+
         app.get("/rest/bookings", (req, res) -> {
-
             var bookings = collection(Booking.class).find();
-
             res.json(bookings);
         });
+
+
+        //removing/deleting bookings (by selecting its ID)
+        app.delete("/rest/bookings/:id",(req,res)->{
+            var id=(req.params("id"));
+            collection(Booking.class).deleteById(id);
+        });
+
+
+        //Adding a new member
+        app.post("/rest/members",(req,res) ->{
+            var member=req.body(Member.class);
+            var savedMember=collection(Member.class).save(member);
+            res.json(savedMember);
+
+        } );
+
+
+        //Delete (cancel membership) member
+        app.delete("/rest/members/:id",(req,res)->{
+            var id=(req.params("id"));
+            collection(Member.class).deleteById(id);
+        });
+
+
+
+
+
         //choose another port so it doesn't collide with VUE port
         app.listen(5000);
     }
