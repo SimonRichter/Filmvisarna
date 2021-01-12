9d5a8504cc9128af6f601f10d78e53035ca0e75b
snabelA00 <ja000411@gmail.com>
2021-01-12 16:29:22 +0100

    added title, date and time of chosen movie. Added find route and filter method to find matching title

---
+++ frontend/src/views/Booking.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="contentGridContainer">
+    <h2>{{ movie.title }}</h2>
+    <span>{{ movie.time }}</span
+    ><span>{{ movie.date }}</span>
+    <!-- Put all content here in grid container -->
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      // get id from url parameter
+      return this.$route.params.title;
+    },
+    movie() {
+      return this.$store.state.movies.filter((obj) => obj.id == this.id)[0];
+    },
+  },
+};
+</script>
+
+<style>
+</style>
