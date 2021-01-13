ffb21c6a12527a579fb4304d1690c5122c502c9a
snabelA00 <ja000411@gmail.com>
2021-01-13 19:54:00 +0100

    changed to the right url path and fetches the chosen movie

--- frontend/src/views/Booking.vue
+++ frontend/src/views/Booking.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="contentGridContainer">
-  
-      <h2>Hello World</h2>
-      <h2>{{ movies.title }}</h2>
-      <span>{{ movies.time }}</span
-      ><span>{{ movies.date }}</span>
+    <div class="movieInfo">
+      <h2>Filmvisarna,<span> Stora Salongen</span></h2><br>
+      <h3>{{ movie.title }}</h3>
+      <span>Wed 11/04</span><span> - 20:45</span>
       <!-- Put all content here in grid container -->
-  
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   computed: {
-    id() {
-      // get id from url parameter
+    title() {
+      // get movie title from url parameter
       return this.$route.params.title;
     },
     movie() {
-      return this.$store.state.movies.filter((obj) => obj.title == this.title)[0];
+      return this.$store.state.movies.filter(
+        (obj) => obj.title == this.title
+      )[0];
     },
   },
 };
 </script>
 
 <style scoped>
-  h2 {
-    grid-area: 1/1/2/3;
-    height: 200px;
-  }
+* {
+  color: white;
+}
+.movieInfo {
+  grid-area: 1/1/2/3;
+}
+h2 {
+  color: white;
+}
+span {
+  color: lightblue;
+  font-size: 14px;
+}
+h3 {
+  color: lightblue;
+}
 </style>
