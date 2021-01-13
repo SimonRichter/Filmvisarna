40dd6704fead5cac717fcd942886a2b6d6dab2e1
snabelA00 <ja000411@gmail.com>
2021-01-13 09:51:50 +0100

    added route to booking view

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
+"use strict";
+
 import { createStore } from 'vuex'
 
 const state = {
-  movies =[],
-  members =[],
-  bookings =[]
+  movies: [],
+  members: [],
+  bookings: []
 }
 
 const mutations = {
@@ -40,46 +42,11 @@ const actions = {
     moviesList = await moviesList.json()
     store.commit('setMovies', moviesList)
   },
-  async fetchMembers(store) {
-    let membersList = await fetch('/rest/member')
-    membersList = await membersList.json()
-    store.commit('setMembers', membersList)
-  },
   async fetchBookings(store) {
     let bookingsList = await fetch('/rest/booking')
     bookingsList = await bookingsList.json()
     store.commit('setBookings', bookingsList)
-  },
-  // in-parameter is a user object user = {name: Anna, email: anna@gmail.com, password: Hej123}
-  // Backend: need to use collection('Klass').insert(Object) to add a new 
-  addMemberToServer(store, newMember) {
-    // Fetch url of the json file. Method = POST because we want to add something new to the json file in the server
-    // Body = convert the object to a json object
-    let response = await fetch('/rest/member', {
-      method: 'POST',
-      body: JSON.stringify(newMember)
-    })
-    // After the new member has been saved to the json file, get it again and commit to the state array -> members
-    newMember = await response.json()
-    store.commit('addMemberToState', newMember)
-  },
-  removeMemberFromServer(store, memberToDelete) {
-    let response = await fetch('/rest/member', {
-      method: 'DELETE',
-      body: JSON.stringify(memberToDelete)
-    })
-    memberToDelete = await response.json()
-    store.commit('removeMemberFromState', memberToDelete)
-  },
-  // Updates the member if they have added or removed a booking in My Page or Confirmed window
-  updateMemberInServer(store, updatedMember) {
-    let response = await fetch('/rest/member', {
-      method: 'PUT',
-      body: JSON.stringify(updatedMember)
-    })
-    member = await response.json()
-    store.commit('updateMemberInState', updatedMember)
-  },
+  }
 }
 
 export default createStore({ state, mutations, actions })
--- frontend/src/views/Booking.vue
+++ frontend/src/views/Booking.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="contentGridContainer">
-    <h2>{{ movie.title }}</h2>
-    <span>{{ movie.time }}</span
-    ><span>{{ movie.date }}</span>
-    <!-- Put all content here in grid container -->
+  
+      <h2>Hello World</h2>
+      <h2>{{ movies.title }}</h2>
+      <span>{{ movies.time }}</span
+      ><span>{{ movies.date }}</span>
+      <!-- Put all content here in grid container -->
+  
   </div>
 </template>
 
@@ -15,11 +18,15 @@ export default {
       return this.$route.params.title;
     },
     movie() {
-      return this.$store.state.movies.filter((obj) => obj.id == this.id)[0];
+      return this.$store.state.movies.filter((obj) => obj.title == this.title)[0];
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+  h2 {
+    grid-area: 1/1/2/3;
+    height: 200px;
+  }
 </style>
