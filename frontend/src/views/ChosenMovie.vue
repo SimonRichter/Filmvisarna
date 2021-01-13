4047eb144296c4b79d93eae2f6e28406db06dc77
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-01-13 11:20:59 +0100

    Update ChosenMovie.vue
    fixes

--- frontend/src/views/ChosenMovie.vue
+++ frontend/src/views/ChosenMovie.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="contentGridContainer">
     <!-- Put all content here in grid container -->
+    
   </div>
+  <router-link to="/"><li>Movies</li></router-link>
+  <button v-on:click="goBack">Return</button>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    goBack: function(){  
+      <router-link to="/"></router-link>
+    }
+  }
 }
 </script>
 
