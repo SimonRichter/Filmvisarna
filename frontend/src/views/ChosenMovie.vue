5085c58ce58e223b03efd88200ecef7e57f51014
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-01-14 10:07:21 +0100

    Update ChosenMovie.vue

--- frontend/src/views/ChosenMovie.vue
+++ frontend/src/views/ChosenMovie.vue
@@ -1,37 +1,43 @@
 <template>
-  <MovieItem
-      v-for="movie of movies"
-      :key="movie.title"
-      :movieTitle="movie.title"
-      :movieGenres="movie.genre"
-      :imageUrl="movie.poster"
-      :movieLength="movie.runtime"
-      :ageClassification="movie.rated"
-      :moviePlot="movie.plot"
-   />
-  <div class="contentGridContainer">
-   <ChosenMovie/>
+<div class="contentGridContainer">
+    <div class="movieInfo">
+      <h2>
+        Filmvisarna,<span> {{ showing.theatre }}</span>
+      </h2>
+      <br />
+      <h3>{{ movie.title }}</h3>
+      <!-- Put all content here in grid container -->
+    </div>
   </div>
-
 
 </template>
 
 <script>
 import MovieItem from '../components/movieItem.vue'
 export default {
- components: {
-      MovieItem
-   },
      computed: {
     id() {
-      return this.$route.params.id
+      // get showing id from url parameter
+      return this.$route.params.id;
     },
-    movies(){
-      return this.$store.state.movies.filter(movies => movies.id == this.id)[0]
-    }
+    title() {
+      // get movie title from url parameter
+      return this.$route.params.title;
+    },
+    movie() {
+      return this.$store.state.movies.filter(
+        (obj) => obj.title == this.title
+      )[0];
+    },
+    showing() {
+      return this.$store.state.showings.filter((obj) => obj.id == this.id)[0];
+    },
   },
-}
+};
 </script>
+
+
+
 
 <style>
 
