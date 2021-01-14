b8e0d03fb38c589ba1b9705c1c120e8f84ec7204
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-01-14 18:39:00 +0100

    Showing test
    Co-Authored-By: Hugo Granath <72066319+mrHugolo@users.noreply.github.com>

--- frontend/src/views/ChosenMovie.vue
+++ frontend/src/views/ChosenMovie.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="contentGridContainer">
-    <!-- Put all content here in grid container -->
+      <div v-for="movie of showings"
+    :key="movie.id"
+    >
+      <template v-if="true">
+        {{movie.date}} - {{movie.time}} Title: {{movie.title}}  Seats available: {{movie.seats}}
+      </template>
+
+    </div>
+    <!-- Put a ll content here in grid container -->
   </div>
 </template>
 
 <script>
 export default {
+
+  computed:{
+    showings(){
+      return this.$store.state.showings
+    }
+  }
 
 }
 </script>
