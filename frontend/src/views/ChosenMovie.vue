ca743a9aab031c9d5e54adfc3f63d3fba31f9e36
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-01-15 13:26:30 +0100

    fixed dates and times.

--- frontend/src/views/ChosenMovie.vue
+++ frontend/src/views/ChosenMovie.vue
@@ -1,13 +1,14 @@
 <template>
 <div class="contentGridContainer">
+
+
     <div class="movieInfo">
         <h2>
-       Filmvisarna:  <span> {{ showing.theatre }}</span>
+       Filmvisarna:  <span> {{ whichTheater.theatre }}</span>
       </h2>
        <img :src="movie.poster" />
-       
-      <br />
-         <div class="infoContainer">
+        </div>
+         <div class="movieInfoContainer">
             <h1>{{ movie.title }}</h1>
             <h3>{{ movie.genre }}</h3>
             <h3><span></span> | {{ movie.length }}</h3>
@@ -19,9 +20,13 @@
            
             <h4>{{ movie.plot.substring(0, 1000).trimRight() }}...</h4>
          </div>
-    </div>
-  </div>
-
+         
+   
+  
+      <div v-for="showing of showings" :key="showing.id">
+        <div class="temp">{{showing.date}} - {{showing.time}} Title: {{showing.title}}  Seats available: {{showing.seats}}  <button class="bookButton" v-on:click="bookingmethod">Book</button> </div>
+      </div>
+</div>
 </template>
 
 <script>
@@ -30,24 +35,29 @@ export default {
      computed: {
     title() {
       // get showing id from url parameter
-      let id =this.$route.params.title;
+      let id =this.$route.params.title.replace('-', '');;
       console.log(id)
       return id;
     },
+    
    movie(){
       let movie = this.$store.state.movies.filter(movie => movie.title == this.title)[0]
       console.log(movie)
       return movie
     },
-    showing() {
+    whichTheater() {
       let showing = this.$store.state.showings.filter((obj) => obj.title == this.title)[0];
       console.log(showing);
       return showing;
+    },
+     showings() {
+      return this.$store.state.showings.filter((obj) => obj.title == this.title);
     },
     allMovies(){
       return this.$store.state.movies;
     },
   },
+  
 };
 </script>
 <style scoped>
@@ -65,25 +75,36 @@ h3 {
    color: rgba(245, 222, 179, 0.432);
 }
 h4 {
-   margin-top: 5px;
+   margin-top: 7px;
+   margin-bottom: 7px;
    color: rgb(50, 114, 136);
+   font-size: 20px;
+   margin-right: 35vh;
 }
 h5{
    font-size: 20px;
 }
 
 .movieiInfo {
-   border-radius: 5px;
    display: flex;
    background-color: rgba(51, 51, 51, 0.39);
-   margin: 0 20px 20px 20px;
    box-shadow: 0 0 5px 0 rgb(59, 81, 105);
+   float: left;
 }
-.infoContainer {
-   margin: 0 20px 0 20px;
+.movieInfoContainer {
+   margin: 50px 20px 200px -450px;
+   float: left;
+   
 }
 .contentGridContainer{
    margin-top: 10vh;
    margin-bottom: 20vh;
+}
+.temp{
+   color:beige;
+}
+.bookButton{
+   display: flex;
+   margin-bottom: 10px;
 }
 </style>
