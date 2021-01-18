06e981730c97737869ba66b3bdda8558088cf659
snabelA00 <ja000411@gmail.com>
2021-01-18 10:41:20 +0100

    navbar fixed position

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="contentGridContainer">
     <!-- Put all content here in grid container -->
     <CarouselList class="carousel" />
-    <h2>In cinemas</h2>
+    <h2 class="inCinemas">In cinemas</h2>
     
     <ShowingList class="showingList" />
   </div>
@@ -29,21 +29,24 @@ export default {
 </script>
 
 <style scoped>
+
 .carousel {
-  grid-area: 1/1/2/4;
+  grid-area: 1/1/2/3;
   /* height: 80vh; */
   background-color: white;
-  /* margin: 2vh 0 2vh; */
+  margin: 2vw 0 7vw;
   color: darkgreen;
 }
-h2 {
+.inCinemas {
+  grid-area: 2/1/3/3;
   color: white;
   border-bottom: 1px #dc0428 solid;
 }
 .showingList {
-  grid-area: 2/1/3/4;
+  grid-area: 3/1/4/3;
   height: auto;
-  margin: 2vh 0 2vh;
+  margin: 1vw 0 5vw;
   display: flex;
+  font-weight: 800;
 }
 </style>
