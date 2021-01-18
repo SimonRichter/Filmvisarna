cfae8ac7123a032fd74a09c6f2f2e8e2a27080ea
SimonRichter <simonmrichter@gmail.com>
2021-01-18 10:39:49 +0100

    wheat

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="contentGridContainer">
-    <!-- Put all content here in grid container -->
-    <CarouselList class="carousel" />
-    <h2>In cinemas</h2>
-    
-    <ShowingList class="showingList" />
-  </div>
+   <div class="contentGridContainer">
+      <!-- Put all content here in grid container -->
+      <CarouselList class="carousel" />
+      <h2>In cinemas</h2>
+
+      <ShowingList class="showingList" />
+   </div>
 </template>
 
 <script>
@@ -13,37 +13,37 @@ import ShowingList from "../components/ShowingList.vue";
 import CarouselList from "../components/CarouselList.vue";
 
 export default {
-  components: {
-    ShowingList,
-    CarouselList,
-  },
-  // data() {
-  //   return {};
-  // },
-  // computed: {
-  //   movies() {
-  //     return this.$store.state.movies;
-  //   },
-  // },
+   components: {
+      ShowingList,
+      CarouselList,
+   },
+   // data() {
+   //   return {};
+   // },
+   // computed: {
+   //   movies() {
+   //     return this.$store.state.movies;
+   //   },
+   // },
 };
 </script>
 
 <style scoped>
 .carousel {
-  grid-area: 1/1/2/4;
-  /* height: 80vh; */
-  background-color: white;
-  /* margin: 2vh 0 2vh; */
-  color: darkgreen;
+   grid-area: 1/1/2/4;
+   /* height: 80vh; */
+   background-color: white;
+   /* margin: 2vh 0 2vh; */
+   color: darkgreen;
 }
 h2 {
-  color: white;
-  border-bottom: 1px #dc0428 solid;
+   color: wheat;
+   border-bottom: 1px #dc0428 solid;
 }
 .showingList {
-  grid-area: 2/1/3/4;
-  height: auto;
-  margin: 2vh 0 2vh;
-  display: flex;
+   grid-area: 2/1/3/4;
+   height: auto;
+   margin: 2vh 0 2vh;
+   display: flex;
 }
 </style>
