a04604516f500b9fce3efbf465436d737b60cf37
bextex <r.sarvegard@gmail.com>
2021-01-18 19:47:15 +0100

    Revert "Revert "Merge branch 'Develop' into feature-color-testing""
    This reverts commit 58d2ba28ae43036ab5f4c622d855ed0bbe764467.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="navBarContainer">
-    <ul class="navBar floatLeft">
-      <router-link to="/" class="floatLeft"><li>Logo</li></router-link>
-    </ul>
-    <ul class="navBar floatRight">
-      <router-link to="/movies"><li>Movies</li></router-link>
-      <router-link to="/about"><li>About</li></router-link>
-
-      <router-link to="/sign-in"><li>Sign in</li></router-link>
-    </ul>
-  </div>
+   <div class="navBarContainer">
+      <ul class="navBar floatLeft">
+         <router-link to="/" class="floatLeft">
+            <li class="firstListItem">
+               <img src="../pictures/logoSmall.png" id="logo" />
+            </li>
+         </router-link>
+      </ul>
+      <ul class="navBar floatRight">
+         <router-link to="/movies"><li>Movies</li></router-link>
+         <router-link to="/about"><li>About</li></router-link>
+         <router-link to="/sign-in"><li>Sign in</li></router-link>
+      </ul>
+   </div>
 </template>
 
 <script>
@@ -43,15 +46,36 @@ export default {};
   
   text-decoration: none;
   padding: 0;
+   /* display: flex;
+   border-bottom: 1px #dc0428 solid; */
+   margin: 0;
+   padding: 0;
+   overflow: hidden;
+   position: fixed;
+   width: 100%;
+   z-index: 1;
 }
+/* .navBar {
+   display: flex;
+   align-items: center;
+   list-style-type: none;
+   background-color: #000000;
+   text-decoration: none;
+   padding: 0;
+} */
 
 .floatRight {
-  width: 50%;
-  justify-content: flex-end;
+   width: 50%;
+   justify-content: flex-end;
 }
 .floatLeft {
-  width: 50%;
-  justify-content: flex-start;
+   width: 50%;
+   justify-content: flex-start;
+}
+.firstListItem {
+   padding: 0;
+   padding-top: 5px;
+   padding-left: 5px;
 }
 
 li {
@@ -67,5 +91,9 @@ li {
 
 a {
    text-decoration: none;
+}
+img {
+   height: 70px;
+   width: auto;
 }
 </style>
