c39eb08281b8e8d4706314f3879d700b2ea0afd5
SimonRichter <simonmrichter@gmail.com>
2021-01-18 16:58:18 +0100

    CSS For ChosenMovie changed

--- frontend/src/views/ChosenMovie.vue
+++ frontend/src/views/ChosenMovie.vue
@@ -1,189 +1,201 @@
 <template>
-  <div class="contentGridContainer">
-    <div class="movieInfo">
-      <h2>
-        <!-- Filmvisarna:  <span> {{ whichTheater.theatre }}</span>-->
-      </h2>
-      <img :src="movie.poster" />
-    </div>
-    <div class="movieInfoContainer">
-      <h1>{{ movie.title }}</h1>
-      <h3>{{ movie.genre }}</h3>
-      <h3><span></span> | {{ movie.length }}</h3>
-      <h3><span></span> | {{ movie.runtime }}</h3>
-      <h3><span></span> | {{ movie.rated }}</h3>
-      <h4>Language: {{ movie.language }}</h4>
-      <h5><span></span> Director: {{ movie.director }}</h5>
-      <h5><span></span> Actors: {{ movie.actors }}</h5>
-
-      <h4>{{ movie.plot.substring(0, 1000).trimRight() }}...</h4>
-    </div>
-
-    <div class="showingList" v-for="showing of showings" :key="showing.id">
-      <div class="temp">
-        <button
-          class="bookButton"
-          @click="
-            $router.push(
-              '/chosen-movie/' + showing.title + '/booking/' + showing.id
-            )
-          "
-        >
-          Book
-        </button>
-        {{ showing.date }} || {{ showing.time }} || {{ showing.theatre }} ||
-        {{ showing.salon }}
+   <div class="setWidth">
+      <div class="movieInfo">
+         <!-- <img :src="movie.posterLandscape" /> -->
+         <iframe
+            width="1220"
+            height="400"
+            src="https://www.youtube.com/embed/kaU2A7KyOu4"
+         >
+         </iframe>
       </div>
-    </div>
-  </div>
+      <h3>{{ movie.genre }}</h3>
+      <h3><span></span> | {{ movie.runtime }}</h3>
+      <h3><span></span> | Rated: {{ movie.rated }}</h3>
+      <h3><span></span> | Language: {{ movie.language }}</h3>
+      <h3><span></span> | Director: {{ movie.director }}</h3>
+      <h3><span></span> | Actors: {{ movie.actors }}</h3>
+      <p>{{ movie.plot.substring(0, 1000).trimRight() }}...</p>
+
+      <div class="showingList" v-for="showing of showings" :key="showing.id">
+         <div class="anotherFormattingDiv">
+            <button
+               class="bookButton"
+               @click="
+                  $router.push(
+                     '/chosen-movie/' + showing.title + '/booking/' + showing.id
+                  )
+               "
+            >
+               <h4>Book</h4>
+            </button>
+            <h4 class="showingItemText">
+               {{ showing.date }} - {{ showing.time }} | {{ showing.theatre }} |
+               {{ showing.salon }}
+            </h4>
+         </div>
+      </div>
+   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    today() {
-      const x = new Date();
-      const year = x.getFullYear();
-      const month = x.getMonth() + 1;
-      const day = x.getDate();
+   computed: {
+      today() {
+         const x = new Date();
+         const year = x.getFullYear();
+         const month = x.getMonth() + 1;
+         const day = x.getDate();
 
-      const date =
-        year +
-        "-" +
-        (month < 10 ? "0" : "") +
-        month +
-        "-" +
-        (day < 10 ? "0" : "") +
-        day;
-      return date;
-    },
+         const date =
+            year +
+            "-" +
+            (month < 10 ? "0" : "") +
+            month +
+            "-" +
+            (day < 10 ? "0" : "") +
+            day;
+         return date;
+      },
 
-    tomorrow() {
-      const today = new Date();
-      const x = new Date(today);
-      x.setDate(x.getDate() + 1);
-      const year = x.getFullYear();
-      const month = x.getMonth() + 1;
-      const day = x.getDate();
+      tomorrow() {
+         const today = new Date();
+         const x = new Date(today);
+         x.setDate(x.getDate() + 1);
+         const year = x.getFullYear();
+         const month = x.getMonth() + 1;
+         const day = x.getDate();
 
-      const tomorrow =
-        year +
-        "-" +
-        (month < 10 ? "0" : "") +
-        month +
-        "-" +
-        (day < 10 ? "0" : "") +
-        day;
+         const tomorrow =
+            year +
+            "-" +
+            (month < 10 ? "0" : "") +
+            month +
+            "-" +
+            (day < 10 ? "0" : "") +
+            day;
 
-      return tomorrow;
-    },
+         return tomorrow;
+      },
 
-    dayAfterTomorrow() {
-      const today = new Date();
-      const x = new Date(today);
-      x.setDate(x.getDate() + 2);
-      const year = x.getFullYear();
-      const month = x.getMonth() + 1;
-      const day = x.getDate();
+      dayAfterTomorrow() {
+         const today = new Date();
+         const x = new Date(today);
+         x.setDate(x.getDate() + 2);
+         const year = x.getFullYear();
+         const month = x.getMonth() + 1;
+         const day = x.getDate();
 
-      const dayAfterTomorrow =
-        year +
-        "-" +
-        (month < 10 ? "0" : "") +
-        month +
-        "-" +
-        (day < 10 ? "0" : "") +
-        day;
+         const dayAfterTomorrow =
+            year +
+            "-" +
+            (month < 10 ? "0" : "") +
+            month +
+            "-" +
+            (day < 10 ? "0" : "") +
+            day;
 
-      return dayAfterTomorrow;
-    },
+         return dayAfterTomorrow;
+      },
 
-    title() {
-      // get showing id from url parameter
-      return this.$route.params.title.replace("-", " ");
-    },
+      title() {
+         // get showing id from url parameter
+         return this.$route.params.title.replace("-", " ");
+      },
 
-    movie() {
-      return this.$store.state.movies.filter(
-        (movie) => movie.title == this.title
-      )[0];
-    },
-    whichTheater() {
-      return this.$store.state.showings.filter(
-        (obj) => obj.title == this.title
-      )[0];
-    },
-    showings() {
-      return this.$store.state.showings
-        .filter((obj) => obj.title == this.title)
-        .filter(
-          (obj) =>
-            obj.date == this.today ||
-            obj.date == this.tomorrow ||
-            obj.date == this.dayAfterTomorrow
-        );
-    },
-    allMovies() {
-      return this.$store.state.movies;
-    },
-  },
+      movie() {
+         return this.$store.state.movies.filter(
+            (movie) => movie.title == this.title
+         )[0];
+      },
+      whichTheater() {
+         return this.$store.state.showings.filter(
+            (obj) => obj.title == this.title
+         )[0];
+      },
+      showings() {
+         return this.$store.state.showings
+            .filter((obj) => obj.title == this.title)
+            .filter(
+               (obj) =>
+                  obj.date == this.today ||
+                  obj.date == this.tomorrow ||
+                  obj.date == this.dayAfterTomorrow
+            );
+      },
+      allMovies() {
+         return this.$store.state.movies;
+      },
+   },
 };
 </script>
+
 <style scoped>
-a {
-  text-decoration: none;
-  color: wheat;
-  margin: 0;
-  padding: 0;
+iframe {
+   border: none;
+   margin-top: 70px;
+   margin-bottom: 10px;
+}
+button {
+   text-align: center;
+   border: #dc0428 1px solid;
+   background-color: rgb(0, 0, 0);
+   border-radius: 5px;
+   color: white;
+   font-family: "Bebas Neue", cursive;
+   width: 60px;
+   padding-left: 10px;
+   margin-top: 7px;
+   margin-bottom: 7px;
+   font-size: 20px;
+}
+button:active,
+button:disabled {
+   background-color: rgb(46, 46, 46);
+}
+button:disabled {
+   cursor: default;
 }
 h1 {
-  margin-top: 10px;
+   margin-top: 5px;
+   margin-bottom: 10px;
 }
 h3 {
-  display: inline;
-  color: rgba(245, 222, 179, 0.432);
+   display: inline;
+   color: rgba(245, 222, 179, 0.432);
 }
 h4 {
-  margin-top: 7px;
-  margin-bottom: 7px;
-  color: wheat;
-  font-size: 20px;
-  margin-right: 35vh;
+   margin-top: 7px;
+   margin-bottom: 7px;
+   color: white;
+   font-size: 20px;
 }
-h5 {
-  font-size: 20px;
+p {
+   margin-top: 10px;
+   margin-bottom: 25px;
 }
 
-.movieiInfo {
-  display: flex;
-  background-color: rgba(51, 51, 51, 0.39);
-  box-shadow: 0 0 5px 0 rgb(59, 81, 105);
-  float: left;
-  grid-area: 1/1/2/2;
-}
-.movieInfoContainer {
-  grid-area: 2/1/3/3;
-  margin-bottom: 10vh;
-}
-.contentGridContainer {
-  margin-top: 10vh;
-  
-}
-.temp {
-  color: beige;
+.movieInfo {
+   display: flex;
+   align-items: center;
+   justify-content: center;
 }
 .showingList {
-
-  grid-column-start: 1;
-  grid-column-end: 3;
-   border-bottom: 1px solid red;
-   padding: 10px 16px;
+   border-top: 1px solid red;
 }
 .bookButton {
-  display: flex;
-  float: left;
-  margin-bottom: 10px;
-  margin-right: 20px;
-  cursor: pointer;
+   margin-bottom: 10px;
+   margin-right: 20px;
+   cursor: pointer;
+}
+.anotherFormattingDiv {
+   padding-top: 5px;
+   padding-bottom: 2px;
+   display: flex;
+   align-items: center;
+}
+.setWidth {
+   width: 80vw;
+   margin: 0 auto;
 }
 </style>
