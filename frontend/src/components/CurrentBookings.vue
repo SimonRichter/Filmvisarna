905ef7eb208e076c0108a2cd314afe8026df21d1
snabelA00 <ja000411@gmail.com>
2021-01-26 09:30:11 +0100

    currentbookings and oldbookings components

---
+++ frontend/src/components/CurrentBookings.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="currentTicketCont" v-for="booking of currentBookings" :key="booking">
+    <div class="whereWhen">
+      <h4>{{ showing.theatre }}, {{ showing.salon }}</h4>
+      <h3 class="dateTime">{{ showing.date }} {{ showing.time }}</h3>
+    </div>
+    <div class="what">
+      <h3>{{ showing.title }}</h3>
+      <div v-for="ticket in currentBookings.tickets" :key="ticket">
+        <h3>{{ ticket.value }} {{ ticket.title }}</h3>
+      </div>
+      <h3>{{ currentBookings.totalSum }} kr</h3>
+     
+      <h4>{{ showing.id }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    showing() {
+      return this.$store.state.bookingInfo.showing;
+    },
+    currentBookings() {
+      this.$store.commit("setBookings", this.$store.state.bookingInfo);
+      console.log("bookings: ", this.$store.state.bookings);
+      return this.$store.state.bookings;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -1,7 +1,20 @@
-<template></template>
+<template>
+  <div class="contentGridContainer">
+    <h2>My Page</h2>
+    <CurrentBookings/>
+    <OldBookings/>
+  </div>
+</template>
 
 <script>
-export default {};
+import CurrentBookings from "../components/CurrentBookings.vue";
+import OldBookings from "../components/OldBookings.vue";
+export default {
+  components: {
+    CurrentBookings,
+    OldBookings
+  },
+};
 </script>
 
 <style scoped>
