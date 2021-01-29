52fa87d863edc16aa337b89dcd6cfc24164c0895
snabelA00 <ja000411@gmail.com>
2021-01-29 18:38:08 +0100

    currentbooking working(almost)

--- frontend/src/components/CurrentBookings.vue
+++ frontend/src/components/CurrentBookings.vue
@@ -1,76 +1,75 @@
 <template>
-   <h1>{{ currentBookings }}</h1>
-   <h2>Current bookings</h2>
-   <div
-      class="currentTicketCont"
-      v-for="booking of currentBookings"
-      :key="booking"
-   >
-      <div class="whereWhen">
-         <h4>{{ showing.theatre }}, {{ showing.salon }}</h4>
-         <h3 class="dateTime">{{ showing.date }} {{ showing.time }}</h3>
+  <h2>Current bookings</h2>
+  <div
+    class="currentTicketCont"
+    v-for="booking of currentBookings"
+    :key="booking"
+  >
+    <div class="whereWhen" v-for="showing of newShowingsArr" :key="showing">
+      <div v-for="index in showing" :key="index">
+        <h4>{{ index.title }}, {{ index.salon }}</h4>
+        <h3 class="dateTime">{{ index.date }} {{ index.time }}</h3>
       </div>
-      <div class="what">
-         <h3>{{ showing.title }}</h3>
-         <h3>{{ booking.tickets }}</h3>
-         <div v-for="ticket in currentBookings.tickets" :key="ticket">
-            <h3>{{ ticket.value }} {{ ticket.title }}</h3>
-         </div>
-         <h3>Total: {{ currentBookings.totalSum }} kr</h3>
-         <h3>Booking ID: {{ showing.id }}</h3>
+    </div>
+    <!-- <div class="what">
+      <h3>{{ showing.title }}</h3>
+      <h3>{{ booking.tickets }}</h3>
+      <div v-for="ticket in currentBookings.tickets" :key="ticket">
+        <h3>{{ ticket.value }} {{ ticket.title }}</h3>
       </div>
-   </div>
+      <h3>Total: {{ booking.totalSum }} kr</h3>
+      <h3>Booking ID: {{ showing.id }}</h3>
+    </div> -->
+  </div>
 </template>
 
 <script>
 export default {
-   computed: {
-      showing() {
-         return this.$store.state.bookingInfo.showing;
-      },
-      userEmail() {
-         return this.$store.state.member.email;
-      },
-      currentBookings() {
-         this.$store.commit("setBookings", this.$store.state.bookingInfo);
-         console.log("BookingInfo: ", this.$store.state.bookingInfo);
-         console.log("Bookings: ", this.$store.state.bookings);
-         console.log(
-            "filteredBookings: ",
-            this.$store.state.bookings.filter((booking) =>
-               booking.userEmail.includes(this.userEmail)
-            )
-         );
-         return this.$store.state.bookings;
-         // .filter((booking) =>
-         //    booking.userEmail.includes(this.userEmail)
-         // );
-      },
-   },
-   // methods: {
-   //    getUserEmail() {
-   //       console.log("Member: ", this.$store.state.member);
-   //       console.log("Email: ", this.$store.state.member.email);
-   //       return this.$store.state.member.email;
-   //    },
-   // },
+  computed: {
+    memberEmail() {
+      return this.$store.state.member.email;
+    },
+    currentBookings() {
+      console.log("this.userEmail computed: ", this.memberEmail);
+      let x = this.$store.state.bookings.filter(
+        (booking) => booking.userEmail == this.memberEmail
+      );
+      console.log("filteredBookings: ", x);
+
+      return this.$store.state.bookings.filter(
+        (booking) => booking.userEmail == this.memberEmail
+      );
+    },
+    newShowingsArr() {
+      const showingsArr = [];
+      for (let i = 0; i < this.currentBookings.length; i++) {
+        let xFind = this.$store.state.showings.filter(
+          (x) => x.id == this.currentBookings[i].showingId
+        );
+        console.log("49 ", xFind);
+        showingsArr.push(xFind);
+      }
+      console.log("52 ", showingsArr);
+      return showingsArr;
+    },
+  },
 };
 </script>
 
 <style scoped>
 h2 {
-   text-align: center;
-   grid-column-start: 1;
-   grid-column-end: 2;
+  text-align: center;
+  grid-column-start: 1;
+  grid-column-end: 2;
 }
 h3 {
-   margin-top: 0px;
+  margin-top: 0px;
 }
 .currentTicketCont {
-   text-align: center;
-   grid-column-start: 1;
-   grid-column-end: 2;
-   grid-row-start: 3;
-   grid-row-end: auto;
+  text-align: center;
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 3;
+  grid-row-end: auto;
 }
 </style>
