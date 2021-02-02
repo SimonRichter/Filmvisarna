d107fef73458d69f156ce7a3dbc4d0a859b2e82c
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-02-02 23:46:07 +0100

    Update About.vue

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,39 +1,6 @@
 <template>
-  <div class="contentGridContainer">
 
-     <div class="map" style="width: 100%; overflow: hidden; height: 350px">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8988.611700266438!2d13.162159345680744!3d55.72109449828493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653bd7809088d27%3A0xf626b08182a4c26e!2zw4VrZXJncsOkbmRlbiwgMjI2IDYwIEx1bmQ!5e0!3m2!1sen!2sse!4v1612132497208!5m2!1sen!2sse"
-        width="100%"
-        height="700"
-        frameborder="0"
-        style="border: 0; margin-top: -150px"
-      >
-      </iframe>
-    </div>
-    
-    <div class="map" style="width: 100%; overflow: hidden; height: 350px">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.856246965763!2d13.20947061592819!3d55.691486980537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465397d9b820d3c5%3A0x3adf01a0cddb0af0!2sVueX%20Cinema!5e0!3m2!1sen!2sse!4v1612137441735!5m2!1sen!2sse"
-        width="100%"
-        height="700"
-        frameborder="0"
-        style="border: 0; margin-top: -150px"
-      ></iframe>
-    </div>
 
-   
-        <div class="address">
-      <h3>Theatre 1</h3>
-      <h4>Kalkstensvägen 3</h4>
-      <h4>224 78 Lund</h4>
-    </div>
-
-    <div class="address">
-      <h3>Theatre 2</h3>
-      <h4>Åkergränden 5</h4>
-      <h4>226 60 Lund</h4>
-    </div>
     <p class="about pFirst">
       Xorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quia.
       Tempore deleniti in deserunt consequuntur excepturi. Sed nobis soluta
@@ -66,8 +33,7 @@
       quae sequi vitae quaerat repudiandae rem iste provident voluptate minima
       ea.
     </p>
-   
-  </div>
+  
 </template>
 
 
