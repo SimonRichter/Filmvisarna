91f6a91446b81cdafda3774346b0fd54c79d35aa
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-02-03 18:27:30 +0100

    Delete account component added

---
+++ frontend/src/components/DeleteMember.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="delButton" @click="delMem();$router.push('/')">Delete Account</button>
+</template>
+
+<script>
+export default {
+  computed: {
+    curMem() {
+      return this.$store.state.member;
+    },
+  },
+
+  methods: {
+    delMem() {
+      fetch("/api/logout");
+      fetch("/rest/members/" + this.curMem.id, {
+        method: "DELETE",
+      });
+      this.$store.commit("logOutMember");
+    
+    },
+  },
+};
+</script>
+
+<style>
+.delButton{
+ 
+  cursor: pointer;
+
+}
+</style>
+
+
