54706ed58a069cc0f59a114f0412f646096a7464
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-02-05 21:59:07 +0100

    cleaning code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -66,17 +66,5 @@ export default {
 </script>
 
 <style scoped>
-@media print {
-  body * {
-    visibility: hidden;
-  }
-  #section-to-print, #section-to-print * {
-    visibility: visible;
-  }
-  #section-to-print {
-    position: absolute;
-    left: 0;
-    top: 0;
-  }
-}
+
 </style>
